8acd1db1a798947e81b2b43bcb146b5c8f07e646
zastria <94423517+zastria@users.noreply.github.com>
2021-11-16 17:09:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // with some specifications added (like almost exact date of release).
 // First entry is the newest while last is the oldest.
 const patchDateMaps = [
-        {version: "2.54.4", date: "Mon Sep 27 2021 18:11:00 GMT"},
-        {version: "2.5", date: "Tue Apr 15 2014 22:00:00 GMT"}
+        {version: "15", date: "Thy Jun 03 2021 16:03:00 GMT"},
+        {version: "2.5", date: "Sat Nov 28 2021 20:38:00 GMT"}
     ]
 
 var updateTimer;
